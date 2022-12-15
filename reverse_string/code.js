c2570a3b1f1eb287b74e08dccfdd31dabe1bb035
Israel Maytorena <israelmaytorena@gmail.com>
2022-12-14 21:31:30 -0600

    prettiefied code

--- reverse_string/code.js
+++ reverse_string/code.js
@@ -1,15 +1,17 @@
-// solutions
+// solution
 function solution(string) {
+    // console.log(string);
     let new_string = "";
-    console.log(string);
-    for (let i = string.length - 1; i >= 0 ; i--) {
-        new_string = new_string + string[i]
-    }
-    console.log(new_string);
-    return new_string
-}
-// entries : results
 
+    // We only need to use a for loop going from the last index and decreasing their value
+    for (let i = string.length - 1; i >= 0; --i) {
+        new_string = new_string + string[i];
+    }
+    // console.log(new_string);
+    return new_string;
+}
+
+// entries : results
 let entries_results = {
     "Cat": "taC",
     "The Daily Byte": "etyB yliaD ehT",
